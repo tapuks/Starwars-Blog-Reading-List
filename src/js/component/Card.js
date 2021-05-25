2de26b24256572f4eb55d6b298a081544ce92aee
tapuks <david_berdiell@hotmail.com>
2021-05-25 20:31:35 +0000

    operator ternary

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -11,20 +11,22 @@ const Card = props => {
 		.catch(error => console.log(error));
 
 	return (
+         
 		<div className="card">
 			<img
 				className="card-img-top"
 				src="https://c4.staticflickr.com/8/7481/27348202211_641e6dafe6.jpg"
 				alt="Card image cap"
 			/>
+          {props.selection = "people" ?  
 			<div className="card-body card-text-center">
-				<h5 className="card-title">{result.name}</h5>
+				<h5 className="card-title">{result.name}</h5> 
 				<p className="card-text ">
 					<li>Gender: {result.gender}</li>
 					<li>Hair-Color: {result.hair_color} </li>
 					<li>Eye-Color: {result.eye_color}</li>
 				</p>
-
+            :22} 
 				<div className="d-flex justify-content-between">
 					<a href="#" className="btn btn-outline-primary">
 						Learn more!
