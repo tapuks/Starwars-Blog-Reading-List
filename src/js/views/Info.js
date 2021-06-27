16356dcb06d5df65f4592041d009e105a05fc21e
tapuks <david_berdiell@hotmail.com>
2021-06-27 10:02:37 +0000

    horizontal-line

--- src/js/views/Info.js
+++ src/js/views/Info.js
@@ -2,6 +2,7 @@ import React, { useContext, useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import PropTypes from "prop-types";
 import { Context } from "../store/appContext";
+import "../../styles/home.scss";
 
 const Info = () => {
 	const params = useParams();
@@ -20,61 +21,32 @@ const Info = () => {
 
 	console.log(params.id);
 	return (
-		<div className="row">
-			<div className="col-1" />
-			<div className="col-5">
-				<img
-					className="card-img-top"
-					src="https://i.pinimg.com/originals/fe/e6/c1/fee6c1fc5fba37827fd45f00df7ee4a1.jpg"
-					alt="Card image cap"
-				/>
+		<div>
+			<div className="row">
+				<div className="col-1" />
+				<div className="col-5">
+					<img
+						className="card-img-top"
+						src="https://i.pinimg.com/originals/fe/e6/c1/fee6c1fc5fba37827fd45f00df7ee4a1.jpg"
+						alt="Card image cap"
+					/>
+				</div>
+				<div className="col-5 text-detail">
+					<h1>{Info.name}</h1>
+					<p>
+						Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+						labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+						laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+						voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat Lorem ipsum
+						dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+						dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+						aliquip ex ea commodo consequat.
+					</p>
+				</div>
+				<div className="col-1"> </div>
 			</div>
-			<div className="col-5 text-detail">
-				<h1>{Info.name}</h1>
-				<p>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
-					et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-					aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat Lorem ipsum dolor sit amet,
-					consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-					enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat.
-				</p>{" "}
-			</div>
-			<div className="col-1" />
-			{/* cambio de tema <hr> */}
-			<hr />
-			<div className="row dataBelow">
-				<div>
-					<h5>Name</h5>
-					<p>{Info.name}</p>
-				</div>
-
-				<div>
-					<h5>Birth Year</h5>
-					<p>{Info.birth_year}</p>
-				</div>
-
-				<div>
-					<h5>Gender</h5>
-					<p>{Info.gender}</p>
-				</div>
-
-				<div>
-					<h5>Height</h5>
-					<p>{Info.height}</p>
-				</div>
-
-				<div>
-					<h5>Skin Color</h5>
-					<p>{Info.skin_color}</p>
-				</div>
-
-				<div>
-					<h5>Eye Color</h5>
-					<p>{Info.eye_color}</p>
-				</div>
-			</div>
+			<hr className="horizontal-line" />
+			<div className="color-red">{Info.name}</div>
 		</div>
 	);
 };
